898d9ea47f287bfe6cc52967d08ad443fcaad33c
leoconst <leoconstantinides@hotmail.com>
2018-01-19 10:30:08 +0000

    Add date and time tests

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import datetime, date, time
 from pathlib import Path
 
 
@@ -26,6 +26,11 @@ MAPPING = {
         },
         'time': datetime(432, 1, 5, 5, 32, 0),
         'list': [(4.1, 0.1), (3, 4, 1), (), (True, False)],
+    },
+    'times': {
+        'date': date(1999, 4, 20),
+        'time': time(13, 37, 59),
+        'datetime': datetime(1, 2, 3, 4, 5, 6),
     },
     'bar': 3.2,
     'special': {
@@ -56,6 +61,11 @@ no = false
 
 [table."b.c"]
 e = 5
+
+[times]
+date = 1999-04-20
+time = 13:37:59
+datetime = 0001-02-03T04:05:06
 
 [special]
 infinity = inf
